18a7dfc348d1cccd7e7a5aecced950c11072b160
희철 <hee@huicheol-ui-MacBookPro.local>
2021-01-22 22:58:24 +0900

    first file

---
+++ jinha_game.py
@@ -0,0 +1,32 @@
+class  Monster :
+    def __init__(self,name,hp,damage, defence):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        self.armor = defence
+        print("{0} 출현! [체력 : {1}   공격력 : {2}   방어력 : {3}]".format(\
+            name,hp,damage,defence))
+
+class  Jinha_ugly :
+    def __init__(self,name,hp,damage, defence):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        self.armor = defence
+        print("{0}의 모험 시작! 진하의 상태 : [체력 : {1}   공격력 : {2}   방어력 : {3}]".format(\
+            name,hp,damage,defence))
+
+class Attack :
+    def __init__(self,name_j,name_m) :
+        self.attack = name_j
+        self.defence = name_m
+        print("{0}, {1}를 공격! ".format(name_j,name_m))
+
+
+
+
+
+
+jinha = Jinha_ugly("진하",50,7,7)
+mon1 = Monster("몬스터1",30,3,5)
+Attack(jinha,mon1)
