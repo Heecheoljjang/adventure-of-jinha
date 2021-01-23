0a93f3bcba9591e69d3b854299931ca23321e22f
희철 <hee@huicheol-ui-MacBookPro.local>
2021-01-23 22:31:32 +0900

    6

--- jinha_game.py
+++ jinha_game.py
@@ -13,32 +13,34 @@ class Jinha:
 
     def Attack(self, name_1, d_a, name_2):
         if d_a <= 0:
+            d_a = 0
             print("공격할수없음")
         else:
             print("{0}, {1}의 데미지로 {2}을 공격".format(name_1, d_a, name_2))
 
     def damaged(self, name, damage):
-        self.hp -= damage
-        if self.hp > 0:
-            print("몬스터는 체력이 {0}남았다.".format(self.hp))
-        else:
-            print("몬스터는 사망했다.")
+        if damage <= 0:
+            damage = 0
+        elif damage > 0:
+            self.hp -= damage
+            if self.hp <= 0:
+                self.hp = 0
+                print("몬스터의 체력이 {0}이 되었다.".format(self.hp))
+                print("몬스터는 사망했다.")
+            else :
+                print("몬스터는 체력이 {0}남았다.".format(self.hp))
 
 
 jinha = Jinha("진하", 20, 7, 7, True)
 mon1 = Jinha("몬스터1", 7, 2, 2, False)
-J_M = jinha.damage-mon1.defence  # 진하의 공격력 - 몬스터의 방어력
-M_J = mon1.damage- jinha.defence  # 몬스터의 공격력 - 진하의 방어력
-if M_J <= 0:
-    M_J = 0
-jinha.Attack(jinha.name, J_M, mon1.name)
-mon1.damaged("몬스터1", J_M)
+jinha.Attack(jinha.name, jinha.damage-mon1.defence, mon1.name)
+mon1.damaged("몬스터1", jinha.damage-mon1.defence)
 print("이번엔 몬스터1이 진하를 공격한다.")
-mon1.Attack(mon1.name, M_J, jinha.name)
-jinha.damaged(jinha.name, M_J)
+mon1.Attack(mon1.name, mon1.defence-jinha.damage, jinha.name)
+jinha.damaged(jinha.name, mon1.defence-jinha.damage)
 print("다시 진하의 공격")
-jinha.Attack(jinha.name, J_M, mon1.name)
-mon1.damaged(mon1.name, J_M)
+jinha.Attack(jinha.name, jinha.damage-mon1.defence, mon1.name)
+mon1.damaged(mon1.name, jinha.damage-mon1.defence)
 
 
 
