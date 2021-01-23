c2169a65bb3608458ce171821e9501066d80bad1
희철 <hee@huicheol-ui-MacBookPro.local>
2021-01-23 14:34:22 +0900

    jinha

--- jinha_game.py
+++ jinha_game.py
@@ -1,32 +1,67 @@
 class  Monster :
-    def __init__(self,name,hp,damage, defence):
+    def __init__(self,name,hp_m,damage, defence):
         self.name = name
-        self.hp = hp
+        self.hp_m = hp_m
         self.damage = damage
-        self.armor = defence
+        self.defence = defence
         print("{0} 출현! [체력 : {1}   공격력 : {2}   방어력 : {3}]".format(\
-            name,hp,damage,defence))
+            name,hp_m,damage,defence))
+
+    def Attack_M(self,name_1,d_a,name_2) :
+        '''self.name_1 = name_1
+        self.name_2 = name_2
+        self.d_a = d_a'''
+        if d_a <= 0 :
+            print("공격할수없음")
+        else :
+            print("{0}, {1}의 데미지로{2}을 공격".format(name_1,d_a,name_2))
+
+    def damaged_M(self,name,damage):
+        self.hp_m -= damage
+        print("몬스터는 체력이 {0}남았다.".format(self.hp_m))
+        if self.hp_m <= 0 :
+            print("몬스터는 사망했다.")
 
 class  Jinha_ugly :
-    def __init__(self,name,hp,damage, defence):
+    def __init__(self,name,hp_j,damage, defence):
         self.name = name
-        self.hp = hp
+        self.hp_j = hp_j
         self.damage = damage
-        self.armor = defence
+        self.defence = defence
         print("{0}의 모험 시작! 진하의 상태 [체력 : {1}   공격력 : {2}   방어력 : {3}]".format(\
-            name,hp,damage,defence))
+            name,hp_j,damage,defence))
 
-'''class Attack :
-    def __init__(self,name_j,name_m) :
-        self.attack = name_j
-        self.defence = name_m
-        print("{0}, {1}를 공격! ".format(name_j,name_m))'''
+    def Attack_J(self,name_1, d_a, name_2) :
+        '''self.name_1 = name_1
+        self.name_2 = name_2
+        self.d_a = d_a'''
+        print("{0}, {1}의 데미지로 {2}을 공격".format(name_1,d_a,name_2))
+
+    def damaged_J(self,name,damage):
+        self.hp_j -= damage
+        print("진하는 체력이 {0}남았다.".format(self.hp_j))
+        if self.hp_j <= 0 :
+            print("진하는 사망해버렸다.")
+
+
+jinha = Jinha_ugly("진하",20,7,7)
+mon1 = Monster("몬스터1",7,2,2)
+J_M = jinha.damage-mon1.defence #진하의 공격력 - 몬스터의 방어력
+M_J = mon1.damage- jinha.defence #몬스터의 공격력 - 진하의 방어력
+print("진하는 몬스터1을 마주쳤다")
+print(J_M)
+Jinha_ugly.Attack_J("진하", J_M, "몬스터1")
+Monster.damaged_M("몬스터1",J_M)
+print("이번엔 몬스터1이 진하를 공격한다.")
+Monster.Attack_M("몬스터1",M_J,"진하")
+Jinha_ugly.damaged_J("진하",M_J)
+print("다시 진하의 공격")
+Jinha_ugly.Attack_J("진하",J_M,"몬스터1")
+Monster.damaged_M("몬스터1",J_M)
 
 
 
 
 
 
-jinha = Jinha_ugly("진하",50,7,7)
-mon1 = Monster("몬스터1",30,3,5)
 
